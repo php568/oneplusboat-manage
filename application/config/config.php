52bfc99cae5bb65576546c1441ea6aac29a33b87
liutengfei1 <liutengfei@shopex.cn>
2019-01-24 22:44:07 +0800

    修改数据库配置

--- application/config/config.php
+++ application/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= 'http://dev.oneplusboat.com/';
+$config['base_url']	= 'http://www.oneplusboat.com/';
 
 /*
 |--------------------------------------------------------------------------
