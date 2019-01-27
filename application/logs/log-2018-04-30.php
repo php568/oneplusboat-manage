e3fb67a3be915eeab6f20c576d199d056d78642c
liutengfei1 <liutengfei@shopex.cn>
2019-01-27 22:16:57 +0800

    调试

--- application/logs/log-2018-04-30.php
+++
@@ -1,5 +0,0 @@
-<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed'); ?>
-
-ERROR - 2018-04-30 00:29:19 --> Severity: Notice  --> Undefined index:  HTTP_ACCEPT_LANGUAGE D:\wwwroot\haiyu\wwwroot\application\controllers\home.php 26
-ERROR - 2018-04-30 09:20:41 --> Severity: Notice  --> Undefined index:  HTTP_ACCEPT_LANGUAGE D:\wwwroot\haiyu\wwwroot\application\controllers\home.php 26
-ERROR - 2018-04-30 09:35:04 --> Severity: Notice  --> Undefined index:  HTTP_ACCEPT_LANGUAGE D:\wwwroot\haiyu\wwwroot\application\controllers\home.php 26
