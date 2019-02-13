9e65df478ddef142194bcadf88f6eaf3109df7e9
liutengfei1 <liutengfei@shopex.cn>
2019-02-14 00:04:32 +0800

    修改14

--- data/template/english/customise.php
+++ data/template/english/customise.php
@@ -5,16 +5,16 @@
 <title>Customise</title>
 <meta name="keywords" content="Wetsuits, Dry suits, Lifejacket/PFDs, Jackets, Rash Vests, Thermals, Base layer, Tops, Bottoms, Gloves, Footwear/Shoes, Headgear/Hats" />
 <meta name="description" content="Found in 2004, Located in Shanghai, LIUS is a performance sailing gear brand. Products range covers dinghys and keelboats personal wear. By using ultra-comfortable materials, keep your movements smooth and natural. In any conditions, LIUS performance gear will be there for the fastest sailors. " />
-<link rel="stylesheet" type="text/css" href="<?=$config['site_templateurl'];?>/css/temp/temp.css"  /> 
+<link rel="stylesheet" type="text/css" href="<?=$config['site_templateurl'];?>/css/temp/temp.css"  />
 </head>
 <body>
 	<div class="content">
-		<header class="bbs_header"> 
+		<header class="bbs_header">
 			<div class="top">
-		
+
 			</div>
 		</header>
-		
+
 		<div class="works">
 		<div>
 		<a href="http://www.liusgear.com/" class="back-home"></a>
@@ -24,12 +24,12 @@
 			<?php foreach ($tmpData as $key => $value) {?>
 			<li>
 				<div class="line_left">
-					<p class="line_title"><?=$value['title']?></p> 
+					<p class="line_title"><?=$value['title']?></p>
 					<p class="line_img">
 						<?=$value['content']?>
 					</p>
 				</div>
-				<div class="line_right"> 
+				<div class="line_right">
 					<p class="line_title"></p>
 					<p class="line_img">
 						<img art="<?=$value['title']?>" title="<?=$value['title']?>" src="<?=$value['spic']?>"/>
@@ -37,22 +37,22 @@
 					<p class="download_url">
 						<a alt="<?=$value['pdf']?>" href="#d-bottom">For more details from PDF </a>
 					</p>
-					
+
 				</div>
 			</li>
 			<?php }?>
-		
+
 			<li style="clear:both;padding-top:20px;height:70px;">
-				
+
 				<a href="http://www.liusgear.com/Web-customise.pdf" target="_blank" class="btn_down"></a>
 				<a href="http://www.liusgear.com/Web-customise.pdf" target="_blank" class="btn_pdf"></a>
 				<a href="#" id="d-bottom" target="_blank"></a>
 			</li>
 		</ul>
-		
+
 		</div>
 		<footer class="bbs_footer"> </footer>
-		
+
 	</div>
 </body>
 </html>
