ecbfe85301d8305e5deb4f93cbc8c0d77976ffbb
liutengfei <tengfei.liu@seedeer.com>
2019-02-14 16:47:18 +0800

    修改22

--- data/template/china/contact-us.php
+++ data/template/china/contact-us.php
@@ -91,6 +91,55 @@
 
 <script charset="utf-8" src="<?=$config['site_templateurl'];?>/layui/layui.all.js"></script>
 <script>window.HELP_IMPROVE_VIDEOJS = false; jQuery = layui.jquery</script>
+<script type="text/javascript" src="http://api.map.baidu.com/api?v=1.5&ak=2b866a6daac9014292432d81fe9b47e3"></script>
 <script charset="utf-8" src="<?=$config['site_templateurl'];?>/js/contact-us.js"></script>
+<script type="text/javascript">
+    var $ = layui.jquery;
+
+    function gotobaidu(type)
+    {
+        if($.trim($("input[name=origin]").val())=="")
+        {
+            alert("请输入起点！");
+            return;
+        }else{
+            if(type==1)
+            {
+                $("input[name=mode]").val("transit");
+                $("#gotobaiduform")[0].submit();
+            }else if(type==2)
+            {
+                $("input[name=mode]").val("driving");
+                $("#gotobaiduform")[0].submit();
+            }
+        }
+    }
+    (function() {
+        var map = new BMap.Map("dituContent");
+        var point = new BMap.Point(121.401427,31.03315);//定义一个中心点坐标
+        map.centerAndZoom(point, 15);
+        map.enableScrollWheelZoom();
+        var marker=new BMap.Marker(point);
+        map.addOverlay(marker);
+        var licontent="<b>上海简泊船舶有限公司</b><br>";
+        licontent+="<span><strong>地址：</strong>中国上海市闵行区银春路1800弄19号201</span><br>";
+        licontent+="<span><strong>联系人：</strong>焦女士</span><br>";
+        licontent+="<span><strong>邮箱：</strong>ellen.jiao@liusgear.com</span><br>";
+        licontent+="<span><strong>电话：</strong>(0086)18621935526</span><br>";
+        licontent+="<span class='input'><strong></strong><input class='outset' type='text' name='origin' value='虹桥火车站'/><input class='outset-but' type='button' value='驾车'  onclick='gotobaidu(2)'/><a class='gotob' href=\"url='http://api.map.baidu.com/direction?destination=latlng:"+marker.getPosition().lat+","+marker.getPosition().lng+"|name:上海简泊船舶有限公司®ion=上海&output=html'\" target='_blank'></a></span>";
+
+        var hiddeninput="<input type=\"hidden\" value=\""+'虹桥火车站'+"\" name=\"region\" /><input type=\"hidden\" value=\"html\" name=\"output\" /><input type=\"hidden\" value=\"driving\" name=\"mode\" /><input type=\"hidden\" value=\"latlng:"+marker.getPosition().lat+","+marker.getPosition().lng+"|name:上海简泊船舶有限公司"+"\" name=\"destination\" />";
+
+        var content1 ="<form id=\"gotobaiduform\" action=\"http://api.map.baidu.com/direction\" target=\"_blank\" method=\"get\">" + licontent +hiddeninput+"</form>";
+
+        var opts1 = { width: 400 };
+
+        var  infoWindow = new BMap.InfoWindow(content1, opts1);
+        marker.openInfoWindow(infoWindow);
+        marker.addEventListener('click',function(){
+            marker.openInfoWindow(infoWindow);
+        });
+    })();
+</script>
 </body>
 </html>
