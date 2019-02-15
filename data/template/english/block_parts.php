f4c486d595f365186b3df146d68c7e2bffd53168
liutengfei <tengfei.liu@seedeer.com>
2019-02-15 12:11:59 +0800

    修改模板名称

--- data/template/english/block_parts.php
+++ data/template/english/block_parts.php
@@ -5,28 +5,28 @@
             <div class="field field-name-field-image-block field-type-image field-label-hidden">
                 <div class="field-items">
                     <div class="field-item">
-                        <a href="<?=site_url('category/contact-us'.$langurl);?>">
+                        <a href="<?=site_url('category/parts'.$langurl);?>">
                             <img typeof="foaf:Image"
                                  src="<?=$config['site_templateurl'];?>/sites/default/files/parts/190x120/OPBS302 Sleeve School Sail.jpg"
                                  width="120" alt="OPBS302 Sleeve School Sail" title="OPBS302 Sleeve School Sail"/>
                         </a>
                     </div>
                     <div class="field-item">
-                        <a href="<?=site_url('category/contact-us'.$langurl);?>">
+                        <a href="<?=site_url('category/parts'.$langurl);?>">
                             <img typeof="foaf:Image"
                                  src="<?=$config['site_templateurl'];?>/sites/default/files/parts/190x120/OPBS303 Eyelet School Sail.jpg"
                                  width="120" alt="OPBS303 Eyelet School Sail" title="OPBS303 Eyelet School Sail"/>
                         </a>
                     </div>
                     <div class="field-item">
-                        <a href="<?=site_url('category/contact-us'.$langurl);?>">
+                        <a href="<?=site_url('category/parts'.$langurl);?>">
                             <img typeof="foaf:Image"
                                  src="<?=$config['site_templateurl'];?>/sites/default/files/parts/190x120/OPBS401 GRP School Daggerboard.jpg"
                                  width="120" alt="OPBS401 GRP School Daggerboard" title="OPBS401 GRP School Daggerboard"/>
                         </a>
                     </div>
                     <div class="field-item">
-                        <a href="<?=site_url('category/contact-us'.$langurl);?>">
+                        <a href="<?=site_url('category/parts'.$langurl);?>">
                             <img typeof="foaf:Image"
                                  src="<?=$config['site_templateurl'];?>/sites/default/files/parts/190x120/OPBS402 GRP School Rudder, Tiller& Tiller Extension.jpg"
                                  width="120" alt="OPBS402 GRP School Rudder, Tiller& Tiller Extension" title="OPBS402 GRP School Rudder, Tiller& Tiller Extension"/>
